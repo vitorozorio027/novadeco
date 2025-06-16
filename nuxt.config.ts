91a5830fd20515f71a3bd7d4735886ea5991c77c
Vitor Ozorio <127207568+vitorozorio027@users.noreply.github.com>
2025-06-16 20:15:47 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,5 +55,12 @@ export default defineNuxtConfig({
         }
       }
     }
+  },
+  
+  // Configuração do head para viewport mobile
+  app: {
+    head: {
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
+    }
   }
 })
